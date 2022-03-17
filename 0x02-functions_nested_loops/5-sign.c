5ab9376a224b7bfac3f19b93d6f37f350168398b
Dannie <izuisrael27@gmail.com>
2022-03-16 17:37:25 -0700

    '

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_sign - print th sign of a number
+ * @n: the int to check
+ * Return: 1 and print + if n is greater than zero
+ * 0 and prints 0 if n is zero
+ * -1 and prints - if n is less than zero
+ */
+int print_sign(int n)
+{
+	i(n > 0)
+	{
+		_putchar('+');
+		return (1);
+	} else if (n == 0)
+	{
+		_putchar('-');
+	}
+		return (-1);
+}
---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+nclude "main.h"
+
+/**
+ * _abs - computes value of an integer
+ * @n: the int to check
+ * Return: the absolute value of int
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+	return (-n);
+}
